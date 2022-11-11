61439169ed0a40f7862bd264b9cfba846eaaf326
Justin Murrah <jdmurrah@gmail.com>
2022-11-10 23:59:27 -0600

    buttons on page dice block being generated

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
+let randomRoll = (min, max) => Math.floor(Math.random() * (max - min) + min);
+let arrayOfDice = [];
+
+$("body").append('<div id="container"></div');
+$("#container").append(arrayOfDice);
+
 class Die {
   constructor() {
-    this.value = value;
+    this.div = $("<div id='die'></div>");
+    $("#container").append(this.div);
+  }
+  roll() {
+    this.value = randomRoll(1, 5);
+    this.div.text(this.value);
+    console.log(this.value);
   }
 }
+console.log(arrayOfDice);
+$("#createdie").click(function () {
+  $("#container").append($("<div id='die'></div>"));
+});
+$("#die").click(function () {
+  console.log("hey");
+});
